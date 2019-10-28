c938547f13d2f43df5824ca3d62daec109682be2
Enrique <garcia@lapp.in2p3.fr>
2019-10-28 17:47:55 +0100

    reorganization of the branch. name modification of files and inside setup, move of all the mchdf5_* test files (Pierre s stuff), and little modifications on the dl0_v1 and v2 scripts so that they are more user friendly and they run better

--- setup.py
+++ setup.py
@@ -6,32 +6,33 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 	long_description = f.read()
 
-
 entry_points = {}
 entry_points['console_scripts'] = ['mchdf5_simtel2r1 = ctapipe_io_mchdf5.converter.mchdf5_simtel2r1:main',
-					'mchdf5v2minselection = ctapipe_io_mchdf5.tools.mchdf5_min_selection:main',
-					'mchdf5v2sliceselection = ctapipe_io_mchdf5.tools.mchdf5_slice_selection:main',
-					'mchdf5v2extractsignaltensor = ctapipe_io_mchdf5.tools.mchdf5_extract_signal_tensor:main',
-					'mchdf5v2transpose = ctapipe_io_mchdf5.tools.mchdf5_transpose:main',
-					'mchdf5v2meansigmasortpixelslice = ctapipe_io_mchdf5.tools.mchdf5_mean_sigma_sort:main',
-					'mchdf5v2meansigmasortslicepixel = ctapipe_io_mchdf5.tools.mchdf5_mean_sigma_sort_slice_pixel:main',
-					'mchdf5v2rangesort = ctapipe_io_mchdf5.tools.mchdf5_range_sort:main',
-					'mchdf5v2sigmameansort = ctapipe_io_mchdf5.tools.mchdf5_sigma_mean_sort:main',
-					'mchdf5v2multiplesort = ctapipe_io_mchdf5.tools.mchdf5_multiple_sort:main',
-					'mchdf5v2storebypixelorslice = ctapipe_io_mchdf5.tools.mchdf5_store_by_pixel_or_slice:main',
-					'mchdf5v2injtabsort = ctapipe_io_mchdf5.tools.mchdf5_injtab_sort:main'
+					'mchdf5_tailcut_dilation_dl0v1 = ctapipe_io_mchdf5.converter.mchdf5_tailcut_dilation_dl0v1:main',
+					'mchdf5_tailcut_dilation_dl0v2 = ctapipe_io_mchdf5.converter.mchdf5_tailcut_dilation_dl0v2:main',
+					'mchdf5v2minselection = ctapipe_io_mchdf5.programs.mchdf5_min_selection:main',
+					'mchdf5v2sliceselection = ctapipe_io_mchdf5.programs.mchdf5_slice_selection:main',
+					'mchdf5v2extractsignaltensor = ctapipe_io_mchdf5.programs.mchdf5_extract_signal_tensor:main',
+					'mchdf5v2transpose = ctapipe_io_mchdf5.programs.mchdf5_transpose:main',
+					'mchdf5v2meansigmasortpixelslice = ctapipe_io_mchdf5.programs.mchdf5_mean_sigma_sort:main',
+					'mchdf5v2meansigmasortslicepixel = ctapipe_io_mchdf5.programs.mchdf5_mean_sigma_sort_slice_pixel:main',
+					'mchdf5v2rangesort = ctapipe_io_mchdf5.programs.mchdf5_range_sort:main',
+					'mchdf5v2sigmameansort = ctapipe_io_mchdf5.programs.mchdf5_sigma_mean_sort:main',
+					'mchdf5v2multiplesort = ctapipe_io_mchdf5.programs.mchdf5_multiple_sort:main',
+					'mchdf5v2storebypixelorslice = ctapipe_io_mchdf5.programs.mchdf5_store_by_pixel_or_slice:main',
+					'mchdf5v2injtabsort = ctapipe_io_mchdf5.programs.mchdf5_injtab_sort:main'
 					]
 
 setup(
 	name='ctapipe_io_mchdf5',
 	packages=find_packages(),
 	version='0.1',
-	description='ctapipe plugin for reading Monte-Carlo files (contains the same informations as Simtel files)',
+	description='ctapipe plugin for reading Monte-Carlo files (contains the same information as simtel files)',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	entry_points = entry_points,
 	install_requires=[
-		'numpy>=1.14.0', 
+		'numpy>=1.14.0',
 		'tables>=3.4.4',
 		'ctapipe',
 		'ctapipe-extra',
