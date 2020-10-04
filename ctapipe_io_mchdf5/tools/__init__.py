ba3623269a630d180c660ed4d01cab0fc7fd2a3d
Enrique <garcia@lapp.in2p3.fr>
2020-10-04 23:10:22 +0100

    comment

--- ctapipe_io_mchdf5/tools/__init__.py
+++ ctapipe_io_mchdf5/tools/__init__.py
@@ -4,16 +4,16 @@
 	Licence : CeCILL-C
 '''
 
-from .telescope_copy import *
+from .telescope_copy import copy_all_tel_without_waveform
 try:
-	from .r1_utils import *
-	from .r1_file import *
+	from .r0_utils import *
+	from .r0_file import *
 	from .dl0_utils import *
 	from .simulation_utils import *
 except:
 	pass
 
-
+# TODO telescope_copy functions need to be upgrated to r0 model
 def copyTelIntrSimuNode(fileOut, fileIn, r1NodeName="r1", docR1Node="Raw data waveform informations of the run", chunkshape=1):
 	'''
 	Create all the telescopes without the waveform
@@ -26,16 +26,16 @@ def copyTelIntrSimuNode(fileOut, fileIn, r1NodeName="r1", docR1Node="Raw data wa
 		chunkshape : shape of the chunk to be used to store the data of waveform and minimum
 	'''
 	copy_all_tel_without_waveform(fileOut, fileIn, r1NodeName=r1NodeName, docR1Node=docR1Node, chunkshape=chunkshape)
-	#Copy the instrument and simulation groups
+	# Copy the instrument and simulation groups
 	try:
 		fileOut.copy_node(fileIn.root.instrument, newparent=fileOut.root, recursive=True)
 	except tables.exceptions.NoSuchNodeError as e:
-		print("analysedInputFileHDF5 : no instrument in the file '",fileNameIn,"'")
+		print("analysedInputFileHDF5 : no instrument in the file '", fileIn, "'")
 		pass
 	try:
 		fileOut.copy_node(fileIn.root.simulation, newparent=fileOut.root, recursive=True)
 	except tables.exceptions.NoSuchNodeError as e:
-		print("analysedInputFileHDF5 : no simulation in the file '",fileNameIn,"'")
+		print("analysedInputFileHDF5 : no simulation in the file '", fileIn, "'")
 		pass
 	
 
